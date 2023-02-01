8ae8d8b307b6a93903db2172fcbbc23b59e19c2d
julianzimmerlin <zim.julian@gmail.com>
2023-02-01 20:40:12 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gym_pybullet_drones',
+    version='0.5.2',
+    install_requires=['numpy', 'Pillow', 'matplotlib', 'cycler', 'gym', 'pybullet', 'stable_baselines3', 'ray[rllib]'],
+    py_modules=[]
+)
